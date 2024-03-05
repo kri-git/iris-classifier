0e74932951907cc2c1d58dfbe366e46ca16dc5a8
kri-git <102227582+kri-git@users.noreply.github.com>
2024-03-05 19:40:28 +0530

    Update to use neumerical input instead of slider input

--- server.py
+++ server.py
@@ -21,14 +21,14 @@ def predict_species(sepal_length, sepal_width, petal_length, petal_width):
 
 def main():
     st.title("Iris Flower Species Prediction")
-    st.write("This is a web interface where the properties of IRIS flowers like sepal and petal properties are taken as input. Based on the input features, the species of the IRIS flower is predicted. The model is trained on the IRIS dataset")    
-    st.write("Please adjust the slider and click on predict to get the species prediction.")
+    st.write("This is a web interface where the properties of IRIS flowers like sepal and petal properties are taken as input. Based on the input features, the species of the IRIS flower is predicted. The model is trained based on the IRIS dataset")
+    st.write("Please input the measurements in centimeters.")
 
     # Input fields for user to input the measurements
-    sepal_length = st.slider("Sepal Length in cm", 4.3, 7.9, 5.0)
-    sepal_width = st.slider("Sepal Width in cm", 2.0, 4.4, 3.0)
-    petal_length = st.slider("Petal Length in cm", 1.0, 6.9, 5.0)
-    petal_width = st.slider("Petal Width in cm", 0.1, 2.5, 0.4)
+    sepal_length = st.number_input("Sepal Length between 4.3cm and 8.0cm", min_value=4.3, max_value=8.0, value=5.0)
+    sepal_width = st.number_input("Sepal Width between 2cm and 4.5cm", min_value=2.0, max_value=4.5, value=3.0)
+    petal_length = st.number_input("Petal Length between 1cm and 7cm", min_value=1.0, max_value=7.0, value=5.0)
+    petal_width = st.number_input("Petal Width between 0.1cm and 2.5cm", min_value=0.1, max_value=2.5, value=1.0)
 
     # Predict the species when the user clicks the button
     if st.button("Predict"):
@@ -37,4 +37,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
